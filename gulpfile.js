0fee88659de68bf2a1d258a1cf42ee6d0738250f
scriptfuture <progratronic@gmail.com>
2018-03-11 18:11:44 +0300

    readme edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-	rename = require('gulp-rename');
-  
-gulp.task('minify', function () {
-  gulp.src('src/cozystyle.js')
-      .pipe(uglify())
-	  .pipe(rename('cozystyle.min.js'))
-      .pipe(gulp.dest('build'))
+    rename = require('gulp-rename');
+
+gulp.task('minify', function() {
+    gulp.src('src/cozystyle.js')
+        .pipe(uglify())
+        .pipe(rename({
+            basename: 'cozystyle',
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('build'))
 });
